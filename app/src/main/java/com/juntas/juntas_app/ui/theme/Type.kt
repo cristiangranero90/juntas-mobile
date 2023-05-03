8bcb424e312005ec263c5ccb7545999263c1a847
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-05-03 10:31:44 -0300

    Raleway font configured

--- app/src/main/java/com/juntas/juntas_app/ui/theme/Type.kt
+++ app/src/main/java/com/juntas/juntas_app/ui/theme/Type.kt
@@ -2,19 +2,61 @@ package com.juntas.juntas_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.juntas.juntas_app.R
 
+val Raleway = FontFamily(
+    Font(R.font.raleway_regular),
+    Font(R.font.raleway_medium),
+    Font(R.font.raleway_bold),
+    Font(R.font.raleway_italic)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Raleway,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    titleSmall= TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
     )
+
+
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
