cc25a89fd42fbe4beabbbdc81c23d9a75173458c
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-05-04 16:32:24 -0300

    BottomBar added to shared components

--- app/src/main/java/com/juntas/juntas_app/shared_components/BottomBar.kt
+++ app/src/main/java/com/juntas/juntas_app/shared_components/BottomBar.kt
@@ -2,29 +2,69 @@ package com.juntas.juntas_app.shared_components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.juntas.juntas_app.R
 
 @Composable
 fun BottomBar(
     modifier: Modifier = Modifier,
 ) {
-    BottomAppBar(
+    val iconsColor = MaterialTheme.colorScheme.onSurface
+
+    NavigationBar(
         modifier = modifier.fillMaxWidth(),
         containerColor = MaterialTheme.colorScheme.background,
-        contentColor = MaterialTheme.colorScheme.onBackground,
         tonalElevation = 1.dp
     ) {
-
-
+        NavigationBarItem(
+            selected = true,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = stringResource(R.string.go_to_home),
+                    tint = iconsColor
+                )},
+            alwaysShowLabel = true,
+            label = { Text(text = stringResource(R.string.home))}
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    painterResource(id = R.drawable.world_pink),
+                    contentDescription = stringResource(R.string.passenger_item),
+                    tint = iconsColor
+                        )},
+            alwaysShowLabel = true,
+            label = { Text(text = stringResource(R.string.passenger))}
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    painterResource(id = R.drawable.car_pink),
+                    contentDescription = stringResource(R.string.driver_item),
+                    tint = iconsColor
+                        )},
+            alwaysShowLabel = true,
+            label = { Text(text = stringResource(R.string.driver))}
+        )
     }
+
 }
 
 @Composable
--- app/src/main/java/com/juntas/juntas_app/ui/theme/Theme.kt
+++ app/src/main/java/com/juntas/juntas_app/ui/theme/Theme.kt
@@ -1,17 +1,14 @@
 package com.juntas.juntas_app.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -26,18 +23,18 @@ private val LightColorScheme = lightColorScheme(
     secondary = PurpleGrey40,
     tertiary = Pink40,
     surface = PinkStrong,
-    onSurface = PinkSmooth
+    onSurface = PinkSmooth,
 
 
-    /* Other default colors to override
+    // Other default colors to override
     background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    //surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    //onSurface = Color(0xFF1C1B1F),
+
 )
 
 @Composable
