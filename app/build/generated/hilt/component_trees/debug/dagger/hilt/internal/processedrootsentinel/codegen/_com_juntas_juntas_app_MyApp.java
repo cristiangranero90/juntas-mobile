f8aa0209ca83c34f2b97e926d3b0655182eb0626
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-06-19 21:27:22 -0300

    deleted files

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_juntas_juntas_app_MyApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.juntas.juntas_app.MyApp"
-)
-public final class _com_juntas_juntas_app_MyApp {
-}
--- app/src/main/java/com/juntas/juntas_app/MyApp.kt
+++
@@ -1,8 +0,0 @@
-package com.juntas.juntas_app
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyApp : Application() {
-}
