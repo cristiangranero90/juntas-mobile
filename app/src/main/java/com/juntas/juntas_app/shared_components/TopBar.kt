f8206e5dcdba112814ca30c6261a600ce14588df
Cristian Gabriel <cristian_granero90@yahoo.com>
2024-01-23 17:02:44 -0300

    Launch icons added. SignOut from profile image is working

--- app/src/main/java/com/juntas/juntas_app/shared_components/TopBar.kt
+++ app/src/main/java/com/juntas/juntas_app/shared_components/TopBar.kt
@@ -7,29 +7,28 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.Coil
 import coil.compose.AsyncImage
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.juntas.juntas_app.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,10 +39,21 @@ fun TopBar(
     imageUrl: String,
     modifier: Modifier = Modifier,
 ) {
+    
+    var open = remember {
+        mutableStateOf(false)
+    }
+    var elements = remember {
+        mutableStateListOf<String>()
+    }
+    elements.add("Close session")
+    
     TopAppBar(
         modifier = modifier.fillMaxWidth(),
         title = {
-            Row(modifier = Modifier.fillMaxWidth().absoluteOffset(x = (-20).dp),
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .absoluteOffset(x = (-20).dp),
                 verticalAlignment = Alignment.Top,
                 horizontalArrangement = Arrangement.Center
             ) {
@@ -64,7 +74,10 @@ fun TopBar(
             Box(modifier = Modifier
                 .size(39.dp)
                 .clip(CircleShape)
-                .clickable { onProfileClicked() }
+                .clickable {
+                    onProfileClicked()
+                    open.value = !open.value
+                }
                 .shadow(elevation = 1.dp, shape = CircleShape),
                 contentAlignment = Alignment.Center
             ) {
@@ -73,13 +86,29 @@ fun TopBar(
                     contentScale = ContentScale.Fit,
                     error = painterResource(R.drawable.ic_launcher_foreground)
                 )
+                DropdownMenu(
+                    expanded = open.value,
+                    onDismissRequest = { open.value = !open.value }
+                ) {
+                    for (element: String in elements) {
+                        Text(
+                            text = element,
+                            modifier = Modifier.clickable {
+                                open.value = !open.value
+                                Firebase.auth.signOut()
+                                //TODO: Repair SignOut
+                            }
+                        )
+                    }
+                }
             }
         },
         actions = {
             Image(
                 painterResource(id = R.drawable.notification_pink ),
                 contentDescription = stringResource(R.string.notifications),
-                modifier = Modifier.clickable { onNotificationsClicked() }
+                modifier = Modifier
+                    .clickable { onNotificationsClicked() }
             )
         },
     )
